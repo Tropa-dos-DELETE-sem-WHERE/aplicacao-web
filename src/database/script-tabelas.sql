e9c53380f6bd5ab375b0a655c95d260e4debd817
MateusSilvaSPTECH <mateus.ssouza@sptech.school>
2025-08-21 11:50:47 -0300

    Web-data-viz configurado para uso do grupo 06

---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,26 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql server
+*/
+
+CREATE DATABASE grupo06;
+
+USE grupo06;
+
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
