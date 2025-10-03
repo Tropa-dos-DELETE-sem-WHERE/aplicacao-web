1256b5cdd400eb87156b77aedb60e81315f60c57
MateusSilvaSPTECH <mateus.ssouza@sptech.school>
2025-10-03 16:14:53 -0300

    Feat - Cadastro do Usuario

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -42,22 +42,26 @@ function autenticar(req, res) {
 }
 
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
+    
+    const nomeInstituicao = req.body.nomeInstituicaoServer;
+    const nomeUsuario = req.body.nomeUsuarioServer;
+    const email = req.body.emailServer;
+    const senha = req.body.senhaServer;
+    const tipoUsuario = req.body.tipoUsuarioServer;
 
     // Faça as validações dos valores
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
+    if (nomeInstituicao == undefined) {
+        res.status(400).send("Nome da Instituição está undefined!");
+    } else if (nomeUsuario == undefined) {
+        res.status(400).send("Nome do Usuário está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
+    } else if (tipoUsuario == undefined) {
+        res.status(400).send("Seu tipo de usuario está undefined!");
     } else {
-
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nomeInstituicao,nomeUsuario,email,senha,tipoUsuario)
             .then(
                 function (resultado) {
                     res.json(resultado);
