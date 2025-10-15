23f0224ed24ba852bfa290dae74dae50137f135c
MateusSilvaSPTECH <mateus.ssouza@sptech.school>
2025-10-15 12:53:30 -0300

    Feat: Inserir,Listar,Deletar professores funcionando

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -40,8 +40,44 @@ function atualizarDadoByUser(idInstituicao,nomeUsuario,email,idUsuario) {
     return database.executar(instrucaoSql);
 }
 
+function listarProfessores(){
+    console.log("acessei o selctall")
+    var instrucaoSql = 
+    `
+        SELECT * FROM usuario where tipoUsuario_id = 2;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function inserirProfessor(nome, email, senha, tipoUsuario, escola_id) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function inserirProfessor(): ", nome, email, senha, tipoUsuario, escola_id);
+
+    var instrucaoSql = `
+        INSERT INTO usuario (nome, email, senha, escola_id, tipoUsuario_id)
+        VALUES ('${nome}', '${email}', '${senha}', ${escola_id},${tipoUsuario});
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function deleteProfessor(idProfessor) {
+    console.log("ACESSEI O USUARIO MODEL \n\n function deleteProfessor(): ", idProfessor);
+
+    var instrucaoSql = `
+        DELETE FROM usuario
+        WHERE id = ${idProfessor};
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
 module.exports = {
     autenticar,
     cadastrar,
-    atualizarDadoByUser
+    atualizarDadoByUser,
+    listarProfessores,
+    inserirProfessor,
+    deleteProfessor
 };
--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 var usuarioController = require("../controllers/usuarioController");
 
-//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
 router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
 })
@@ -14,6 +13,18 @@ router.post("/autenticar", function (req, res) {
 
 router.put("/atualizarDadoByUser", function (req, res) {
     usuarioController.atualizarDadoByUser(req, res);
+});
+
+router.post("/inserirProfessor", function(req, res) {
+    usuarioController.inserirProfessor(req, res);
+});
+
+router.get("/listarProfessores", function(req, res) {
+    usuarioController.listarProfessores(req, res);
+});
+
+router.delete("/deleteProfessores", function(req, res) {
+    usuarioController.deleteProfessor(req, res);
 });
 
 module.exports = router;
